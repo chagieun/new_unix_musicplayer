368c792962b2b6378f27e94dac391b717ddc0e78
chagieun <90942759+chagieun@users.noreply.github.com>
2022-06-04 02:10:41 +0900

    Update textfilewriter.c

--- libs/textfilewriter.c
+++ libs/textfilewriter.c
@@ -6,13 +6,31 @@
 #include "textfilewriter.h"
 
 void create_music_titles(FILE* stream){
+  stream = fopen("title.txt", "w+");
+
+  int num;
+  scanf("%d", &num);
+  for(int i=0; i<num; i++){
+         read_file("title.txt");
+  }
+  stream = fopen("title.txt", "a+");
+  write_file("title.txt");
+  fclose(stream);
 
 }
 
 void read_file(char* file_name){
-
+	char buffer[MAX_TITLE_SIZE];
+	FILE* stream = fopen(file_name, "a+");
+	fgets(buffer, sizeof(buffer), stdin);
+	fputs(buffer, stream);
+	fclose(stream);
 }
 
 void write_file(char* file_name){
-
+	FILE* stream = fopen(file_name, "a");
+	char musicTitle[51];
+	fgets(musicTitle, sizeof(musicTitle), stdin);
+	fputs(musicTitle, stream);
+	fclose(stream);
 }
